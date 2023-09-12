43ef8653ed6229ff963f6d94d657620e5418ebe4
Antas Jain <jainantas.99@gmail.com>
2023-09-12 18:43:22 -0400

    Exploring yarn functionalities Read, Remove, List, Add

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const yargs = require("yargs");
 const command = process.argv[2]
-console.log(process.argv);
+//console.log(process.argv);
 //customize yargs version
 yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe:'Add a new note',
+    handler: function(){
+        console.log('adding a new note!')
+    }
+})
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe:'Remove an existing note',
+    handler: function(){
+        console.log('Removing the note!')
+    }
+})
+yargs.command({
+    command: 'read',
+    describe:'Read an existing note',
+    handler: function(){
+        console.log('Reading the note!')
+    }
+})
+yargs.command({
+    command: 'list',
+    describe:'Listing all existing notes',
+    handler: function(){
+        console.log('Notes list here!')
+    }
+})
 console.log(yargs.argv);
-if(command === 'add')
-{
-    console.log('Adding Note!')
-}else if(command === 'remove'){
-    console.log('Removing Note!')
-}
